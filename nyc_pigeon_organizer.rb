321e8ba38829037fd7e90d58cb34262bd9dbf8b7
Kosi Akporji <kosiazom@gmail.com>
2020-04-30 03:00:11 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_data = { }
+  data.each do |key, value|
+    value.each do |color, names|
+      names.each do |name|
+        if !new_data[name] 
+       new_data[name] = { }
+        end
+        if !new_data[name][key]
+          new_data[name][key] = [ ]
+        end
+        new_data[name][key]<< color.to_s
+      end
+    end
+  end
+  new_data
 end
